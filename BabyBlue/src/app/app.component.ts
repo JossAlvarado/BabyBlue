e1c98886c5ca714f639edec9619f2cebbefdc3ed
JossAlvarado <josseline.alvaradovargas@utxicotepec.edu.mx>
2023-10-17 18:10:27 -0600

    Angular

---
+++ BabyBlue/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'BabyBlue';
+  user = {
+    email: '',
+    password: ''
+  };
+
+  login() {
+    // Aquí puedes agregar la lógica para iniciar sesión
+    console.log('Iniciar sesión');
+    console.log('Email:', this.user.email);
+    console.log('Contraseña:', this.user.password);
+  }
+}
